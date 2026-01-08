9abb9b08e4eb1a6738c7bc8c99d393e7e29210fb
RITIKYADAV0070 <ritikrao673@gmail.com>
2026-01-08 19:40:51 +0530

    Fix data imports inside lib/data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,5 @@
-
-import companies from "@/data/companies.json";
-import jobs from "@/data/jobs.json";
-import pages from "@/data/pages.json";
+import companies from "../data/companies.json";
+import jobs from "../data/jobs.json";
+import pages from "../data/pages.json";
 
 export { companies, jobs, pages };
